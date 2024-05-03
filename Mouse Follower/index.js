18dcf2b736e5818ef87abf969ad4b683e68a407e
Jawhar Hmidi <168595636+jawharhmidi02@users.noreply.github.com>
2024-05-03 02:46:10 +0100

    Add files via upload

---
+++ Mouse Follower/index.js
@@ -0,0 +1,87 @@
+/* ******************************* Classes ******************************* */
+
+class Particule{
+    constructor(x, y, radius, color){
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.radian = Math.random() * Math.PI *2;
+        this.oldPoints = {x: x, y: y};
+        this.lastPoints = {x: x, y: y};
+        
+    }
+    draw(){
+        ctx.beginPath();
+        ctx.strokeStyle = this.color;
+        ctx.lineWidth = this.radius;
+        ctx.moveTo(this.oldPoints.x, this.oldPoints.y);
+        ctx.lineTo(this.x, this.y);
+        ctx.stroke();
+
+    }
+    update(){
+        this.oldPoints.x = this.x;
+        this.oldPoints.y = this.y;
+
+        this.lastPoints.x += (mouse.x - this.lastPoints.x) ;
+        this.lastPoints.y += (mouse.y - this.lastPoints.y) ;
+
+        this.x = this.lastPoints.x + Math.cos(this.radian) ;
+        this.y = this.lastPoints.y + Math.sin(this.radian) ;
+        this.draw();
+    }
+}
+
+/* ******************************* Functions ******************************* */
+
+function animate(){
+    requestAnimationFrame(animate);
+
+    ctx.fillStyle = 'rgba(255,255,255,0.1)'
+    ctx.fillRect(0,0,innerWidth,innerHeight);
+
+    particles.forEach(particle => {
+        particle.update();
+    });
+}
+
+function getDistance(x1, y1, x2, y2){
+    let xDistance = x2 - x1;
+    let yDistance = y2 - y1;
+    return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
+}
+
+document.addEventListener('mousemove', function(event){
+    mouse.x = event.x;
+    mouse.y = event.y;
+})
+
+function rgb(){
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    return `rgb(${r},${g},${b})`;
+}
+
+/* ******************************* Implimentations ******************************* */
+
+const canvas = document.getElementById('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const ctx = canvas.getContext('2d');
+
+var mouse = {x: canvas.width/2, y: canvas.height/2};
+
+var particles = [];
+for(let i = 0; i < 1; i++){
+    let x = canvas.width/2;
+    let y = canvas.height/2;
+    let radius = Math.random() * 3 +1;
+    let color = rgb();
+
+    particles.push(new Particule(x, y, radius, color));
+}
+
+animate();
